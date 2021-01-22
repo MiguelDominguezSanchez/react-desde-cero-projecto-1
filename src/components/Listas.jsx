bc09907a372b22247b504932abf084284537ab1d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-22 10:15:49 +0100

    29. Operador de propagación

--- src/components/Listas.jsx
+++ src/components/Listas.jsx
@@ -9,12 +9,18 @@ const Listas = () => {
 
 	const [lista, setLista] = useState(estadoInicial);
 
+	const agregarElemento = () => {
+		console.log('click');
+		setLista([...lista, { id: 4, texto: 'tarea 4' }]);
+	};
+
 	return (
 		<div>
 			<h2>Listas</h2>
 			{lista.map((item, index) => (
-				<h4 key={item.id}>{item.texto}</h4>
+				<h4 key={index}>{item.texto}</h4>
 			))}
+			<button onClick={() => agregarElemento()}>Agregar</button>
 		</div>
 	);
 };
