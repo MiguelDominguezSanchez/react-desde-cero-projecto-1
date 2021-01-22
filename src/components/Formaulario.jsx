4b4d71c6b794fc12039ffc1edf99b8b6d3be859e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-22 10:45:13 +0100

    31. React Developers Tools

--- src/components/Formaulario.jsx
+++ src/components/Formaulario.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Formaulario = () => {
+	const [fruta, setFruta] = React.useState('');
+	const [Descripcion, setdescripcion] = React.useState('');
+
 	return (
 		<div>
 			<h2>Formulario</h2>
@@ -15,7 +18,7 @@ const Formaulario = () => {
 					placeholder="Ingrese Descripción"
 					className="form-control mb-2"
 				/>
-				<button className="btn btn-primary btn-block">Agregar</button>
+				<button className="btn btn-primary w-100">Agregar</button>
 			</form>
 		</div>
 	);
