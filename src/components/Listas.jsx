7993cba56c06ea6fbb9983f4819dc70b8752648a
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-22 09:59:13 +0100

    28. Array de objetos

--- src/components/Listas.jsx
+++ src/components/Listas.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 
 const Listas = () => {
-	const estadoinicial = [1, 2, 3, 4, 5];
+	const estadoInicial = [
+		{ id: 1, texto: 'tarea 1' },
+		{ id: 2, texto: 'tarea 2' },
+		{ id: 3, texto: 'tarea 3' },
+	];
 
-	const [lista, setLista] = useState(estadoinicial);
+	const [lista, setLista] = useState(estadoInicial);
 
 	return (
 		<div>
 			<h2>Listas</h2>
 			{lista.map((item, index) => (
-				<h4 key={index}>{item}</h4>
+				<h4 key={item.id}>{item.texto}</h4>
 			))}
 		</div>
 	);
